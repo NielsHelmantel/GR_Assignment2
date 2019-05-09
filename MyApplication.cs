eb7e967058dda8fe6b981d8b0fce14748ce064ff
Niels Helmantel <nielshelmantel@gmail.com>
2019-05-09 16:38:45 +0200

    added ray class

--- MyApplication.cs
+++ MyApplication.cs
@@ -1,19 +1,48 @@
 namespace Template
 {
+
 	class MyApplication
 	{
 		// member variables
 		public Surface screen;
-		// initialize
+        public Sprite sprite;
+        public Ray ray;
+        public [] light;
+		
+        // initialize
 		public void Init()
 		{
+
+            for(int m = 0; m )
+
+            for(int x = 0; x < screen.pixels.Length; x++)
+            {
+                screen.pixels[x] = MixColor(0,0,0);
+
+                for(int y = 0; y < light.Length; y++)
+                {
+
+                }
+            }
+
+            
+
 		}
 		// tick: renders one frame
 		public void Tick()
 		{
-			screen.Clear( 0 );
+			// screen.Clear( 0 );
 			screen.Print( "hello world", 2, 2, 0xffffff );
 			screen.Line( 2, 20, 160, 20, 0xff0000 );
-		}
-	}
+
+            screen.Bar(50, 50, 100, 100, 155);
+
+        }
+
+        int MixColor(int red, int green, int blue)
+        {
+            return (red << 16) + (green << 8) + blue;
+        }
+
+    }
 }
---
+++ Ray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INFOGR2019Tmpl8
+{
+    class Ray
+    {
+        public int O;
+        public int D;
+        public int t;
+
+        public void pixelPositions()
+        {
+            return; 
+        }
+
+        public void normalizeDirectionToLight()
+        {
+            return;
+        }
+
+        public void distanceToLight()
+        {
+            return;
+        }
+    }
+}
